a6e823a45f311dcf5beb1c86e9fb37f3f46b74b5
Yueyi <yueyizhuang.2022@smu.edu.sg>
2023-10-05 00:50:44 +0800

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -26,9 +26,163 @@
     <section class="rooms-section">
         <div class="container">
             <div class="row">
-                <RoomList />
-
-                <!-- <div class="col-lg-12">
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-1.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-2.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-3.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-4.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-5.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <a class="room-box" href="#">
+                        <div class="room-item">
+                            <img src="img/room/room-6.jpg" alt="">
+                                <div class="ri-text">
+                                    <h4>Premium King Room</h4>
+                                    <h3>$159<span>/Perunit</span></h3>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td class="r-o1">Store:</td>
+                                                <td class="r-o2">FairPrice Tampines Hub</td>
+                                            </tr>
+                                            <tr>
+                                                <td class="r-o1">Location:</td>
+                                                <td class="r-o2">1 Tampines Walk, #B1-01 Our Tampines Hub, Singapore 528523b</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="heart-icon" onclick="toggleHeart(this)">
+                                    <i class="fa fa-heart"></i>
+                                </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-12">
                     <div class="room-pagination">
                         <a href="#">1</a>
                         <a href="#">2</a>
@@ -43,16 +197,9 @@
 
 
 <script>
-import HeaderSection from '../components/headerSection.vue';
-import RoomList from '../components/roomList.vue';
-
 export default {
-    components: {
-        HeaderSection,
-        RoomList
-    },
-};
 
+}
 </script>
 
 <style scoped>
