8608d53c39fa528edb19f38fd30d6d04c4745dbb
Yueyi <yueyizhuang.2022@smu.edu.sg>
2023-10-07 02:17:26 +0800

    message

--- src/main.js
+++ src/main.js
@@ -15,6 +15,16 @@ import "../public/css/style.css";
 
 
 
+
+import "../public/css/style.css";
+import "../public/css/bootstrap.min.css";
+import "../public/css/font-awesome.min.css" 
+import "../public/css/elegant-icons.css";
+import "../public/css/flaticon.css";
+import "../public/css/nice-select.css";
+import "../public/css/slicknav.min.css";
+import "../public/css/style.css";
+
 createApp(App).use(store).use(router).mount('#app')
 import { createApp } from 'vue'
 import App from './App.vue'
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,27 @@
 <template>
-     <!-- Offcanvas Menu Begin -->
-     <!-- ... offcanvas menu code ... -->
+    <!-- Offcanvas Menu Begin -->
+    <!-- Offcanvas Menu Overlay -->
     <div class="offcanvas-menu-overlay"></div>
+    <!-- Canvas Open Button -->
     <div class="canvas-open">
         <i class="icon_menu"></i>
     </div>
+    <!-- Offcanvas Menu Wrapper -->
     <div class="offcanvas-menu-wrapper">
+        <!-- Canvas Close Button -->
         <div class="canvas-close">
             <i class="icon_close"></i>
         </div>
+        <!-- Search Icon -->
         <div class="search-icon search-switch">
             <i class="icon_search"></i>
         </div>
+        <!-- Header Configure Area -->
         <div class="header-configure-area">
+            <!-- Profile Option -->
             <div class="profile-option">
                 <span class="bk-btn">Profile <i class="fa fa-user"></i></span>
+                <!-- Profile Dropdown -->
                 <div class="profile-dropdown">
                     <ul>
                         <li><a href="#">Login</a></li>
@@ -30,96 +37,15 @@
                 <li><a href="./byCommunity.html">By Community</a></li>
             </ul>
         </nav>
-        <div id="mobile-menu-wrap"></div>  
-       
+        <div id="mobile-menu-wrap"></div>
     </div>
     <!-- Offcanvas Menu Section End -->
 
-    <!-- Header Section Begin-->
-    <header class="header-section header-normal fixed-top">
-        <!-- ... header code ... -->
-        <div class="top-nav">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <ul class="tn-left">
-                            <li class="col-lg-2">
-                                <a href="./bestDeals.html">
-                                    <img src="img/logo.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <form action="">
-                                    <div class="p-1 bg-light rounded rounded-pill shadow-sm" style="height:40px">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text border-0 bg-light">
-                                                    <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
-                                                </span>
-                                            </div>
-                                            <input type="search" placeholder="Search" aria-describedby="button-addon1" class="form-control border-0 bg-light">
-                                            <div class="input-group-append">
-                                                <span class="input-group-text border-0 bg-light">
-                                                    <button id="button-addon1" type="submit" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </li> 
-                        </ul>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="tn-right">
-                            <div class="profile-option">
-                                <span class="bk-btn">Profile <i class="fa fa-user"></i></span>
-                                <div class="profile-dropdown">
-                                    <ul>
-                                        <li><a href="#">Login</a></li>
-                                        <li><a href="#">Register</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="menu-item">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="nav-menu">
-                            <nav class="mainmenu">
-                                <ul>
-                                    <li class="active">
-                                        <a href="./bestDeals.html">
-                                            <i class="fa fa-money custom-size-icon"></i>
-                                            <span>Best Deals</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="./bySupermarkets.html">
-                                            <i class="fa fa-shopping-basket custom-size-icon"></i>
-                                            <span>By Supermarkets</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="./byCommunity.html">
-                                            <i class="fa fa-users custom-size-icon"></i>
-                                            <span>By Community</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                            <FilterModal /> <!-- Filter Modal -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- Header End -->
+
+    <!-- Header Section Begin -->
+    <HeaderSection />
+    <!-- Header Section Begin -->
+
 
     <!-- Breadcrumb Section Begin -->
     <div class="breadcrumb-section">
@@ -135,12 +61,12 @@
     </div>
     <!-- Breadcrumb Section End -->
 
-        <!-- Rooms Section Begin -->
-        <section class="rooms-section">
+    <!-- Rooms Section Begin -->
+    <section class="rooms-section">
         <div class="container">
             <div class="row">
                 <RoomList />
-    
+
                 <!-- <div class="col-lg-12">
                     <div class="room-pagination">
                         <a href="#">1</a>
@@ -156,19 +82,16 @@
 
 
 <script>
-import FilterModal from '../components/filterModal.vue';
+import HeaderSection from '../components/headerSection.vue';
 import RoomList from '../components/roomList.vue';
 
 export default {
-  components: {
-    FilterModal,
-    // ToggleHeart,
-    RoomList
-  },
+    components: {
+        HeaderSection,
+        RoomList
+    },
 };
 
 </script>
 
-<style>
-
-</style>
+<style></style>
