30bc4b94f58afa5ca175c4152b996a4c0960bfd5
charmaine <147848025+charmainetann@users.noreply.github.com>
2023-11-08 02:11:12 +0800

    Update PreferencePage.vue

--- src/views/PreferencePage.vue
+++ src/views/PreferencePage.vue
@@ -1,209 +1,199 @@
-<template>
-  <HeaderSection/>
-  <div id="app" class="container-fluid body">
-    <form class="row" @submit.prevent="addPreference">
-      <div class='col-2'></div>
-      <div class="col-8 box">
-        <h2>Set Your Preferences</h2>
+import { createStore } from 'vuex'
+import router from '../router'
+import { auth, db } from '../firebase'
+import {
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth'
+import {doc, setDoc, getDoc} from 'firebase/firestore'
 
-        <label for="address" class="form-label">Address:</label>
-        <input type="text" class="form-control w-75" id="address" name="address" 
-                placeholder="Enter Your Home Address" >
-
-        <div class = 'row'>
-          <div class="category col-6">
-            <span>Product Category:</span>
-            <div v-for="category in food_categories" :key="category" name="category" @change="updateSelectedCategories(category)">
-              <input type="checkbox"  :value="category" :id="category" :checked="isCategoryChecked(category)">
-              <label :for="category">{{category}}</label>
-            </div>
-          </div>
-  
-          <div class="deals col-6">
-            <span>Preferred Deals:</span>
-              <div v-for="deal_type in deal_types" :key="deal_type" @change="updateSelectedDealTypes(deal_type)">
-                <input type="checkbox" :value="deal_type" :id="deal_type" :checked="isDealChecked(deal_type)" > 
-                <label :for="deal_type">{{deal_type}}</label>
-              </div>
-          </div>
-        </div>
-
-        <button type="submit" class="btn btn-dark">Submit</button>
-      </div>
-
-      <div class='col-2'></div>
-
-    </form>
-
-  </div>
-</template>
-
-<script>
- import { useStore } from 'vuex';
-import { ref, onMounted } from 'vue';
-import HeaderSection from '../components/headerSection.vue';
-import { doc, getDoc, setDoc } from "firebase/firestore";
-import { auth, db } from '../firebase';
-
-export default {
-  components: { HeaderSection },
-  data() {
-    return {
-      home_address: "",
-      food_categories: ['Bakery', 'Beer, Wine & Spirits', 'Diary, Chilled & Eggs', 'Drinks',
-                'Food Cupboard', 'Frozen', 'Fruits', 'Meat & Seafood', 'Pet Supplies',
-                'Rice, Noodles & Cooking Ingredients', 'Snacks & Confectionery', 'Vegetables'],
-      deal_types: ['Percentage discounts', 'Price discounts',
-                '1 for 1 deals', '"X for $Y" deals'],
-      addressInput: "",
-    };
+  const store = createStore({
+  state: {//Indicates the states of properties like users
+    user:null,
+    authIsReady:false
   },
-  setup() {
-    const store = useStore();
-    const addressInput = ref(null);
-    const selectedCategories = ref([]);
-    const selectedDealTypes = ref([]);
+  mutations: {// functions here mutates the properties of state's properties
 
-    const updateSelectedCategories = (category) => {
-      if (!selectedCategories.value.includes(category)) {
-        selectedCategories.value.push(category);
-      } else {
-        selectedCategories.value = selectedCategories.value.filter((c) => c !== category);
-      }
-    };
-    const updateSelectedDealTypes = (dealType) => {
-      if (!selectedDealTypes.value.includes(dealType)) {
-        selectedDealTypes.value.push(dealType);
-      } else {
-        selectedDealTypes.value = selectedDealTypes.value.filter((d) => d !== dealType);
-      }
-    };
+    SET_USER (state, user) {
+      state.user = user
+    },
+    CLEAR_USER (state){
+      state.user = null
+    },
+    SET_AUTHREADY (state, payload){
+      state.authIsReady = payload
+    }
 
-    // Fetch and update preferences from Firebase
-    onMounted(async () => {
+  },
+  actions: { // various functions that could COMMIT different functions in the mutations
+
+    //checking of login before committing and changing user state
+    async login ({commit}, details){
+      const {email, password} = details
       try {
-        const userDoc = await getDoc(doc(db, "users", auth.currentUser.uid));
-        if (userDoc.exists()) {
-          // Update user preferences
-          item.value.email = userDoc.data().email;
-          item.value.address = userDoc.data().homeaddress;
-          item.value.catpref = Array.isArray(userDoc.data().catpref) ? userDoc.data().catpref : [];
-          item.value.dealpref = Array.isArray(userDoc.data().dealpref) ? userDoc.data().dealpref : [];
-          selectedCategories.value = item.value.catpref;
-          selectedDealTypes.value = item.value.dealpref;
-          addressInput.value = item.value.address;
-          localStorage.setItem('userPreferences', JSON.stringify({
-            address: addressInput.value,
-            selectedCategories: selectedCategories.value,
-            selectedDealTypes: selectedDealTypes.value,
-          }));
-          
+        // needs to await auth(declared data storage inside), as well as checking of email and password
+        await signInWithEmailAndPassword(auth, email, password)
+      } catch(error) {
+        // Triggers various types which are alerted out
+        switch(error.code){
+          case 'auth/invalid-login-credentials':
+            alert("Invalid login credentials")
+            break
+          default:
+            alert("Something went wrong")
         }
-      } catch (error) {
-        console.error('Error retrieving data from Firebase:', error);
+        //return used here since the commit sentence later is when user successfully login
+        return 
       }
-      var autocomplete = new google.maps.places.Autocomplete(document.getElementById("address"));
-        autocomplete.setComponentRestrictions({
-            country: ["sg"]
-      });
-      autocomplete.addListener("place_changed", () => {
-          addressInput.value = document.getElementById("address").value;
-      });
-      // Save preferences in local storage
-      const storedPreferences = localStorage.getItem('userPreferences');
-      if (storedPreferences) {
-        const parsedPreferences = JSON.parse(storedPreferences);
-        addressInput.value = parsedPreferences.address
-        selectedCategories.value = parsedPreferences.selectedCategories;
-        selectedDealTypes.value = parsedPreferences.selectedDealTypes;
-        document.getElementById("address").value = addressInput.value;
-      }
-    });
+      commit('SET_USER', auth.currentUser)
+      const user_data = await getDoc(doc(db,"users",auth.currentUser.uid))
+      const user_rec = user_data.data()
+      
+      localStorage.setItem('userID', auth.currentUser.uid)
+      localStorage.setItem('userType', user_rec.type)
+      localStorage.setItem('userEmail', user_rec.email)
+      localStorage.setItem('homeAddress', user_rec.homeaddress)
 
-    const addPreference = () => {
-      addressInput.value = document.getElementById("address").value;
-      const data = {
-        address: addressInput.value,
-        categories: selectedCategories.value,
-        dealTypes: selectedDealTypes.value,
-      };
-      store.dispatch('addPreference', data);
-    };
-
-    const isCategoryChecked = (cat) => {
-        if (selectedCategories.value && selectedCategories.value.includes(cat)) {
-            return true
+      if (user_rec.type === "consumer"){
+        if (user_rec.address == "" || user_rec.catpref == "" || user_rec.dealpref ==""){
+          router.push('/questionaire')
+        } else {
+          router.push("/") 
         }
+      } else {
+        localStorage.setItem('storeName', user_rec.storename)
+        router.push("/storehome")
+      }
+
+    },
+
+    //checking of register before committing and changing user state
+    async register ({commit}, details){
+      const { email, password} = details
+      try {
+        await createUserWithEmailAndPassword(auth, email, password)
+      } catch(error) {
+        switch(error.code){
+          case 'auth/email-already-in-use':
+            alert("Email already in use")
+            break
+          case 'auth/invalid-email':
+            alert("Invalid Email")
+            break
+          case 'auth/operation-not-allowed':
+            alert("Operation not allowed")
+            break
+          case 'auth/weak-password':
+            alert("Weak password")
+            break
+          default:
+            alert("Something went wrong")
+        }
+        return 
+      }
+      await setDoc(doc(db, "users", auth.currentUser.uid),{
+        email: auth.currentUser.email,
+        type: 'consumer',
+        homeaddress: '',
+        catpref: '',
+        dealpref: '',
+        like:[],
+        cart:[]
+      })
+      commit('SET_USER', auth.currentUser)
+
+      const user_data = await getDoc(doc(db,"users",auth.currentUser.uid))
+      const user_rec = user_data.data()
+
+      localStorage.setItem('user', JSON.stringify(user_rec.type));
+
+      if (user_rec.type === "consumer"){
+        if (user_rec.address == "" || user_rec.catpref == "" || user_rec.dealpref ==""){
+          router.push('/questionaire')
+        } else {
+          router.push("/home")
+        }
+      } else {
+        router.push("/storehome")
+      }
+
+    },
+
+    async addPreference ({commit}, details){
+      const { address, categories, dealTypes} = details
+
+        try {
+          const userDoc = doc(db, 'users', auth.currentUser.uid);
+          const userData = await getDoc(userDoc);
+      
+          if (userData.exists()) {
+            // User document already exists, update it
+            await setDoc(userDoc, {
+              homeaddress: address,
+              catpref: categories,
+              dealpref: dealTypes,
+            }, { merge: true }); // Use merge to update only specified fields
+          }}
+        catch(error) {
+          console.log(error)
+        }
+      
+      commit('SET_USER', auth.currentUser)
+
+      const user_data = await getDoc(doc(db,"users",auth.currentUser.uid))
+      const user_rec = user_data.data()
+
+      if (user_rec.type === "consumer"){
+        console.log(user_rec.homeaddress)
+        console.log(user_rec.catpref)
+        console.log(user_rec.dealpref)
+        const messages = [];
+        if (user_rec.homeaddress == "") {
+          messages.push("Please enter your home address.");
+        }
+        if (user_rec.catpref == "") {
+          messages.push("Please enter product category preference.");
+        }
+        if (user_rec.dealpref == "") {
+          messages.push("Please enter preferred deals.");
+        }
+        if (messages.length === 0) {
+          router.push("/");
+        } 
         else {
-            return false
+          alert(messages.join("\n"));
         }
-    };
-    const isDealChecked = (deal) =>  {
-      if (selectedDealTypes.value && selectedDealTypes.value.includes(deal)) {
-          return true
-      }
-      else {
-          return false
-      }
-    };
+      } 
+      
+    },
 
-    return {
-      addPreference,
-      updateSelectedCategories,
-      updateSelectedDealTypes,
-      isCategoryChecked,
-      isDealChecked,
-    };
-  },
-};
+    async logout({commit}){
+      await signOut(auth)
+      localStorage.removeItem('userID');
+      localStorage.removeItem('userType');
+      localStorage.removeItem('userEmail');
+      localStorage.removeItem('homeAddress');
+      if(localStorage.getItem('storeName')){
+        localStorage.removeItem('storeName');
+      }
+      commit('CLEAR_USER') 
+      router.push('/')
+    },
 
+  }
+})
 
-</script>
+const onsub = onAuthStateChanged(auth,(user) =>{
+  store.commit('SET_AUTHREADY', true)
+  if(user === null){
+    store.commit('CLEAR_USER')
+  } else {
+    store.commit ('SET_USER', user)
+  }
+  onsub()
+}) 
 
-<style scoped>
-      .body {
-          background-color: rgba(241, 193, 201, 0.39);
-      }
-
-      form {
-        padding: 7rem 1rem 3rem;
-      }
-  
-      div {
-          font-family: cursive;
-          font-size: 18px;
-      }
-  
-      .box {
-          background-color: rgb(255, 250, 250);
-          border: 2px solid #2e460b;
-      }
-  
-      h1 {
-          padding: 10px 50px;
-          margin-left: 60px;
-          font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-          font-weight: bold;
-          font-size: 60px;
-          color: rgba(67, 42, 21, 0.984);
-      }
-  
-      h2 {
-          text-align: center;
-          padding: 15px 0px;
-          font-size: 40px;
-      }
-  
-      .category,.deals {
-          padding-left: 20px;
-          padding-bottom: 20px;
-      }
-  
-      input[type=text],.btn {
-          margin-bottom: 20px;
-      }
-  
-      label {
-          padding-left: 10px;
-      }
-  </style>
+//export
+export default store
