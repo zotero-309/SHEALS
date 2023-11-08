ddd7213de98436c122dc691bdb2674883245225f
yushenthun <137487781+yushenthun@users.noreply.github.com>
2023-11-09 00:12:39 +0800

    user local storage for register

--- src/store/index.js
+++ src/store/index.js
@@ -107,8 +107,11 @@ import {doc, setDoc, getDoc} from 'firebase/firestore'
       const user_data = await getDoc(doc(db,"users",auth.currentUser.uid))
       const user_rec = user_data.data()
 
-      localStorage.setItem('user', JSON.stringify(user_rec.type));
-
+      localStorage.setItem('userID', auth.currentUser.uid)
+      localStorage.setItem('userType', user_rec.type)
+      localStorage.setItem('userEmail', user_rec.email)
+      localStorage.setItem('homeAddress', user_rec.homeaddress)
+      
       if (user_rec.type === "consumer"){
         if (user_rec.address == "" || user_rec.catpref == "" || user_rec.dealpref ==""){
           router.push('/questionaire')
