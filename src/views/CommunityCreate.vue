86f00916b111eb8eb3b4c6ff554f55153553fdbd
Yueyi <yueyizhuang.2022@smu.edu.sg>
2023-11-09 23:25:23 +0800

    fixed modal z index

--- src/views/CommunityCreate.vue
+++ src/views/CommunityCreate.vue
@@ -1,45 +1,46 @@
 <template>
-    <HeaderSection/>
+    <HeaderSection />
     <div class="container">
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Add Deal</h1>
-    </div>
+        <div
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 mt-2 pb-2 mb-3 border-bottom">
+            <h1 class="h2 mt-5">Add Deal</h1>
+        </div>
 
-    <button @click="scanmodal(); startscan();" class="btn btn-warning">
-        <span>Scan Now</span>
-    </button>
-
-    <div v-show="scan" class="modal-container">
-    <div class="modal-background" @click="scanmodal"></div>
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title mx-auto">Please positon your barcode</h5>
-        <button type="button" class="close" @click="scanmodal">
-          <span aria-hidden="true">&times;</span>
+        <button @click="scanmodal(); startscan();" class="btn btn-warning">
+            <span>Scan Now</span>
         </button>
-      </div>
-      <div class="modal-body">
-        <div ref="pdtscan" class="scanner"></div>
-      </div>
-    </div>
-    </div>
+
+        <div v-show="scan" class="modal-container">
+            <div class="modal-background" @click="scanmodal"></div>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title mx-auto">Please positon your barcode</h5>
+                    <button type="button" class="close" @click="scanmodal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div ref="pdtscan" class="scanner"></div>
+                </div>
+            </div>
+        </div>
 
 
-    <form @submit.prevent="upload_deal()">
-        
-        <div class="row">
-            <div class="col-lg-4 col-12">
-                <label for="barcodeid" class="py-2">Barcode ID</label>
-                <input type="number" class="form-control" id="barcodeid" ref="barcodeid" readonly>                    
-            </div>
-            <div class="col-lg-4 col-12">
-                <label for="pdtname" class="py-2">Product Name</label>
-                <input type="text" class="form-control" id="pdtname" ref="productname" readonly>                    
-            </div>
-            <div class="col-lg-4 col-12">
-                <input type="text" ref="foodtag" id="foodtag" v-show="false">
-                <label for="pdtcat" class="py-2">Product Category</label>
-                <select id="pdtcat" ref="foodcategory" class="form-control mb-2" required="required">
+        <form @submit.prevent="upload_deal()">
+
+            <div class="row">
+                <div class="col-lg-4 col-12">
+                    <label for="barcodeid" class="py-2">Barcode ID</label>
+                    <input type="number" class="form-control" id="barcodeid" ref="barcodeid" readonly>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <label for="pdtname" class="py-2">Product Name</label>
+                    <input type="text" class="form-control" id="pdtname" ref="productname" readonly>
+                </div>
+                <div class="col-lg-4 col-12">
+                    <input type="text" ref="foodtag" id="foodtag" v-show="false">
+                    <label for="pdtcat" class="py-2">Product Category</label>
+                    <select id="pdtcat" ref="foodcategory" class="form-control mb-2" required="required">
                         <option>Bakery</option>
                         <option>Diary, Chilled & Eggs</option>
                         <option>Drinks</option>
@@ -51,48 +52,49 @@
                         <option>Meat & Seafood</option>
                         <option>Food Cupboard</option>
                         <option>Pet Supplies</option>
-                </select>                   
-            </div>         
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <label for="dealname" class="py-2">Deal Name</label>
-                <input type="text" class="form-control" id="dealname" ref="dealname" placeholder="e.g. Hot deals for soju!"  required>                    
+                    </select>
+                </div>
             </div>
-            <div class="col-6">
-                <label for="deal_type" class="py-2" ref="dealtype">Select Deal Type</label>
+            <div class="row">
+                <div class="col-6">
+                    <label for="dealname" class="py-2">Deal Name</label>
+                    <input type="text" class="form-control" id="dealname" ref="dealname"
+                        placeholder="e.g. Hot deals for soju!" required>
+                </div>
+                <div class="col-6">
+                    <label for="deal_type" class="py-2" ref="dealtype">Select Deal Type</label>
                     <select id="deal_type" ref="dealtype" class="form-control mb-2" required="required">
                         <option>Discounts</option>
                         <option>Bundle Deals</option>
                         <option>1 For 1</option>
                     </select>
-            </div>
+                </div>
 
-        </div>
+            </div>
 
             <div class="row">
                 <div class="col-lg-2 col-sm-4">
                     <label for="dealprice" class="py-2">Price</label>
-                    <input type="number" step="0.01" class="form-control" id="dealprice" ref="dealprice" required> 
+                    <input type="number" step="0.01" class="form-control" id="dealprice" ref="dealprice" required>
                 </div>
                 <div class="col-lg-2 col-sm-4">
                     <label for="dealexpiry" class="py-2">Expiry Date</label>
-                    <input type="date" class="form-control" id="dealexpiry" ref="dealexpiry" required>  
+                    <input type="date" class="form-control" id="dealexpiry" ref="dealexpiry" required>
                 </div>
                 <div class="col-lg-2 col-sm-4">
                     <label for="dealqty" class="py-2">Quantity</label>
-                    <input type="number" class="form-control" id="dealqty" ref="dealqty" required>  
+                    <input type="number" class="form-control" id="dealqty" ref="dealqty" required>
                 </div>
                 <div class="col-lg-6 col-12">
                     <label for="dealplace" class="py-2">Deal's Location</label>
-                    <input type="text" class="form-control" id="dealplace" ref="dealplace" required> 
+                    <input type="text" class="form-control" id="dealplace" ref="dealplace" required>
                 </div>
 
             </div>
             <div class="row">
                 <div class="col-lg-6 col-12">
                     <div class="form-group">
-                        <label for="dealdescr" class="py-2" >Deal Description</label>
+                        <label for="dealdescr" class="py-2">Deal Description</label>
                         <textarea class="form-control" id="dealdescr" ref="dealdescr" rows="6"></textarea>
                     </div>
                 </div>
@@ -104,21 +106,18 @@
 
 
             <button class="btn btn-primary my-3" type="submit">Submit form</button>
-</form>
-</div>
-    
-        
-        
+        </form>
+    </div>
 </template>
 
 <script>
- 
+
 import HeaderSection from '../components/headerSection.vue'
-import  Quagga  from '../assets/quagga.min.js'
+import Quagga from '../assets/quagga.min.js'
 import axios from 'axios'
-import  { storage, db } from '../firebase/index.js'
-import {addDoc, updateDoc, collection, doc} from 'firebase/firestore'
-import { ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+import { storage, db } from '../firebase/index.js'
+import { addDoc, updateDoc, collection, doc } from 'firebase/firestore'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 
 
 
@@ -126,17 +125,17 @@ export default {
     components: {
         HeaderSection
     },
-    data () {
+    data() {
         return {
             //var if the modal pops up or not
             scan: false,
             addressInput: "",
-            autocomplete: null, 
+            autocomplete: null,
         }
     },
 
     methods: {
-        initialiseAutocomplete(){
+        initialiseAutocomplete() {
             // Ensure this method is called after the Google Maps API has loaded
             if (window.google && window.google.maps && window.google.maps.places) {
                 this.autocomplete = new google.maps.places.Autocomplete(this.$refs.dealplace);
@@ -151,7 +150,7 @@ export default {
                 console.error("Google Maps API is not loaded yet.");
             }
         },
-        async upload_deal () {
+        async upload_deal() {
 
             //extracting image
             let dealImgName = this.$refs.dealimg.files[0].name
@@ -163,7 +162,7 @@ export default {
             let storename = localStorage.getItem('storeName')
 
             // add document to products collection (with extracting photo)
-            await addDoc(collection(db, "deals"),{
+            await addDoc(collection(db, "deals"), {
                 barcode: this.$refs.barcodeid.value,
                 product_name: this.$refs.productname.value,
                 product_category: this.$refs.foodcategory.value,
@@ -174,26 +173,26 @@ export default {
                 deal_expiry: this.$refs.dealexpiry.value,
                 deal_price: parseFloat(this.$refs.dealprice.value),
                 deal_quantity: parseInt(this.$refs.dealqty.value),
-                uploaded_by: {email: userEmail, type:userType, name: userEmail.split("@")[0]},
+                uploaded_by: { email: userEmail, type: userType, name: userEmail.split("@")[0] },
                 location: this.$refs.dealplace.value,
-                image_name:dealImgName
+                image_name: dealImgName
             }).then(docRef => {
                 const storageRef = ref(storage, `deals/${userEmail}/${docRef.id}/${dealImgName}`)
-                uploadBytes(storageRef, this.$refs.dealimg.files[0]).then(async(snapshot) => {
+                uploadBytes(storageRef, this.$refs.dealimg.files[0]).then(async (snapshot) => {
                     let dealImgURL = await getDownloadURL(snapshot.ref)
                     console.log(dealImgURL)
-                    await updateDoc(doc(db,"deals",docRef.id),{image:dealImgURL})
+                    await updateDoc(doc(db, "deals", docRef.id), { image: dealImgURL })
                 })
             })
 
-            this.$router.push({name:'Home'})
+            this.$router.push({ name: 'Home' })
 
         },
-        scanmodal () {
+        scanmodal() {
             //func if the modal pops up or not
             this.scan = !this.scan
         },
-        startscan(){
+        startscan() {
             // Upon clicking on 'scan now', quagga lib triggers
             //Initialize Quagga
             Quagga.init({
@@ -210,48 +209,48 @@ export default {
                     console.log(err);
                     return
                 }
-            console.log("Initialization finished. Ready to start");
-            Quagga.start();
+                console.log("Initialization finished. Ready to start");
+                Quagga.start();
             });
         }
-        
+
     },
-     mounted(){
+    mounted() {
         // Initialise autocomplete func
         this.initialiseAutocomplete()
 
         // arrow function used to refer to correct 'this' aka this vue. Using Normal function 'this' refers to quagga lib
         Quagga.onDetected((data) => {
-        var barcodeValue = data.codeResult.code;
-        this.$refs.barcodeid.value = barcodeValue;
-        
-        // Stop Quagga after a barcode is detected
-        Quagga.stop();
+            var barcodeValue = data.codeResult.code;
+            this.$refs.barcodeid.value = barcodeValue;
+
+            // Stop Quagga after a barcode is detected
+            Quagga.stop();
 
 
-        // Close the modal after a barcode is detected
-        if (this.scan) {
-            this.scanmodal();
- 
-        }
+            // Close the modal after a barcode is detected
+            if (this.scan) {
+                this.scanmodal();
 
-        //using axios to get pdt name and cat
-        axios.get(`https://world.openfoodfacts.org/api/v2/product/${barcodeValue}.json`)
-            .then(response => {
-                console.log(response.data.product.categories);
-                let fooditem = response.data.product
-                let foodcat = response.data.product.categories
-                fooditem = fooditem.product_name
+            }
 
-                this.$refs.productname.value = fooditem
-                this.$refs.foodtag.value = foodcat
-                
+            //using axios to get pdt name and cat
+            axios.get(`https://world.openfoodfacts.org/api/v2/product/${barcodeValue}.json`)
+                .then(response => {
+                    console.log(response.data.product.categories);
+                    let fooditem = response.data.product
+                    let foodcat = response.data.product.categories
+                    fooditem = fooditem.product_name
 
-            })
-            .catch( error => {
-                console.error(error);
-            });
-  });
+                    this.$refs.productname.value = fooditem
+                    this.$refs.foodtag.value = foodcat
+
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        });
     }
 
 
@@ -260,7 +259,7 @@ export default {
 
 
 <style scoped>
-video  {
+video {
     width: 300px;
     height: 300px;
     margin: auto;
@@ -280,14 +279,15 @@ video  {
     Modal
     -----------------------*/
 .modal-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1001;
 }
 
 .modal-background {
@@ -298,28 +298,52 @@ video  {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+    /*modal background is below modal content */
     /* Semi-transparent black background */
 }
 
 .modal-content {
+        position: relative;
+        z-index: 1002;
+        /* Ensure the modal content is above the modal background */
+        background: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+        max-width: 500px;
+        width: 100%;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        font-family: "Cabin", sans-serif;
+    }
 
-    background-color: white !important;
-    padding: 20px;
-    max-width: 600px;
-    width: 100%;
-    max-height: 80%;
-    overflow-y: auto;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    z-index: 1000;
-    /* Ensure it's above other elements */
-}
+    /* Close button styles */
+    .modal-header .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        color: #393939;
+        /* Close button color */
+        font-size: 30px;
+        cursor: pointer;
+    }
 
-.modal-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #e0e0e0;
-}
+    /* List item styles */
+    .modal-body ul {
+        list-style: none;
+        padding: 0;
+    }
 
+    .modal-body li {
+        margin-bottom: 10px;
+        font-size: 16px;
+    }
 
+    .modal-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid #e0e0e0;
+    }
 </style>
